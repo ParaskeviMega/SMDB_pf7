308f6754b07140b741906957b6d4995cca07c35e
ParaskeviMega <vivi-mega1@hotmail.com>
2021-12-17 14:26:20 +0200

    create project

---
+++ src/main/java/com/pf7/smdb/SMDB_pf7/SmdbPf7Application.java
@@ -0,0 +1,16 @@
+package com.pf7.smdb.SMDB_pf7;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmdbPf7Application {
+	private static final Logger logger = LoggerFactory.getLogger(SmdbPf7Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmdbPf7Application.class, args);
+		logger.info("Hello");
+	}
+}
---
+++ src/test/java/com/pf7/smdb/SMDB_pf7/SmdbPf7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pf7.smdb.SMDB_pf7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmdbPf7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
