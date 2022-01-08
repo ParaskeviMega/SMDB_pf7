6e63d8e918ac6a6176cc2625667a70bd2439417f
ParaskeviMega <vivi-mega1@hotmail.com>
2022-01-08 13:02:47 +0200

    Movie Controller csv

--- src/main/java/com/pf7/smdb/controller/MovieController.java
+++ src/main/java/com/pf7/smdb/controller/MovieController.java
@@ -6,7 +6,15 @@ import com.pf7.smdb.service.MovieService;
 import com.pf7.smdb.service.PersonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -49,5 +57,32 @@ public class MovieController extends AbstractController<Movie> {
     @GetMapping(params = {"year","rating"})
     public List<Movie> getMoviesByPersonName(@RequestParam("year") Integer year,@RequestParam("rating") String rating){
         return movieService.findMoviesByMovieYearAndMovieRatingStartsWith(year,rating);
+    }
+
+    @GetMapping("/export")
+    public String exportToCSV(HttpServletResponse response) throws IOException {
+        response.setContentType("text/csv");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=movies_" + currentDateTime + ".csv";
+        response.setHeader(headerKey, headerValue);
+
+        List<Movie> listMovies = movieService.findAll();
+
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String[] csvHeader = {"ID", "Title", "Description", "Year", "Genre", "Rating", "Roles"};
+        String[] nameMapping = {"id", "movieTitle", "movieDescription", "movieYear", "movieGenre", "movieRating", "moviePersonRoles"};
+
+        csvWriter.writeHeader(csvHeader);
+
+        for (Movie movie : listMovies) {
+            csvWriter.write(movie, nameMapping);
+        }
+
+        csvWriter.close();
+
+        return "Rows Exported: " + listMovies.size();
     }
 }
